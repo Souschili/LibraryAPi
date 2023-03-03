125174bfe6de0d69d4cf844be974b1214cb49316
Orkhan <oa@wakeupdata.com>
2023-03-03 23:59:52 +0400

    Add project files.

---
+++ MyTestEFCore/EFCore/Data/AppDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace MyTestEFCore.NewFolder.Data
+{
+    public class AppDbContext:DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options):base(options) { }
+        
+
+    }
+}
