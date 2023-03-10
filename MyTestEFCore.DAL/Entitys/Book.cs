7a2cb741e1dffad0bb38a4b786d630febb203cca
Orkhan <oa@wakeupdata.com>
2023-03-11 03:11:04 +0400

    add book entity

---
+++ MyTestEFCore.DAL/Entitys/Book.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MyTestEFCore.DAL.Entitys
+{
+    internal class Book
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(250)]
+        public string? BookName { get; set; }
+
+        [Required]
+        public int Pages { get; set; }
+
+    }
+}
