552f5b7b99db5e1852629495982a6a08d9ca35e8
Orkhan <oa@wakeupdata.com>
2023-03-11 03:09:00 +0400

    add enttity

---
+++ MyTestEFCore.DAL/Entitys/Author.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MyTestEFCore.DAL.Entitys
+{
+    internal class Author
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [StringLength(250)]
+        public string FullName { get; set; }
+        [Column(TypeName ="date")]
+        public DateTime BirthDay { get; set; }
+       
+    }
+}
