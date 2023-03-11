5832f964f06a479301838cd2c7407865699dcb85
Orkhan <oa@wakeupdata.com>
2023-03-11 23:31:37 +0400

    add generic interface

---
+++ MyTestEFCore.DAL/Repository/IGenericRepository.cs
@@ -0,0 +1,11 @@
+﻿namespace MyTestEFCore.DAL.Repository
+{
+    internal interface IGenericRepository<T>
+    {
+        IEnumerable<T> GetAll();
+        IEnumerable<T> GetById(int id);
+        Task Add(T entity);
+        Task Update(T entity);
+
+    }
+}
