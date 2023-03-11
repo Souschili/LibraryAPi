76ac4214eb953115e44a048c309f51dd2ca79770
Orkhan <oa@wakeupdata.com>
2023-03-12 01:58:04 +0400

    install design and update packages

--- MyTestEFCore.DAL/Repository/Repositories/BookRepository.cs
+++ MyTestEFCore.DAL/Repository/Repositories/BookRepository.cs
@@ -1,11 +1,6 @@
 ﻿using MyTestEFCore.DAL.Context;
 using MyTestEFCore.DAL.Entitys;
 using MyTestEFCore.DAL.Repository.IRepositories;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace MyTestEFCore.DAL.Repository.Repositories
 {
