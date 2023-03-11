75cfbff5dcc6d376c5654819f8c617d0e1d47b6d
Orkhan <oa@wakeupdata.com>
2023-03-12 03:40:39 +0400

    add controller

---
+++ MyTestEFCore/Controllers/AuthorController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MyTestEFCore.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthorController : ControllerBase
+    {
+        public AuthorController() { }
+
+        [HttpPost("Author")]
+        public IActionResult AddAuthor(string name)
+        {
+            return Ok();
+        }
+    }
+}
